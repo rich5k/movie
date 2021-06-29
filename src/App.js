2dc5bab461b92bce7601a20f6fe998b2d79aafaa
rich5k <richardanatsui@gmail.com>
2021-06-29 23:57:08 +0100

    created footer js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Footer from './Footer';
 import Navbar from './Navbar';
 import './tailwindcss/styles.css';
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <h1 class="bg-red-100">Start of Movie Rater App</h1>
+      <div class="flex flex-col h-screen">
+        <Navbar></Navbar>
+        <div class="flex-grow">
+          <h1 class="bg-red-100">Start of Movie Rater App</h1>
+
+        </div>
+        <Footer/>
+
+      </div>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import './tailwindcss/styles.css';
+const Footer = () => {
+    return ( 
+        <div className="Footer">
+            <footer class="flex flex-col text-white bg-blue-400 h-12">
+                <div class="my-4">
+                    © Copyright 2021 Richard Anatsui
+
+                </div>
+            </footer>
+        </div>
+     );
+}
+ 
+export default Footer;
