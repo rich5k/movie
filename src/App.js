f845246ab701728fd1dc6bcad7505e65a34930e4
rich5k <richardanatsui@gmail.com>
2021-06-29 23:30:48 +0100

    created navbar js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
 import './tailwindcss/styles.css';
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1 class="bg-red-100">Start of Movie Rater App</h1>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+      <div className="Home">
+        <h1 class="bg-red-100">Start of Movie Rater Home</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
