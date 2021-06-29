5b7113cd4a907703a038265c55a9a72fc8fd63bd
rich5k <richardanatsui@gmail.com>
2021-06-30 00:31:27 +0100

    added movie cards to homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import Footer from './Footer';
-import Navbar from './Navbar';
-import './tailwindcss/styles.css';
+import Home from './Home';
+
 function App() {
   return (
     <div className="App">
-      <div class="flex flex-col h-screen">
-        <Navbar></Navbar>
-        <div class="flex-grow">
-          <h1 class="bg-red-100">Start of Movie Rater App</h1>
-
-        </div>
-        <Footer/>
-
-      </div>
+      <Home/>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,40 @@
+import Footer from './Footer';
+import Navbar from './Navbar';
+import './tailwindcss/styles.css';
+import img from './img/mulan.jpg';
+import img2 from './img/soul.jpg';
 function Home() {
     return (
       <div className="Home">
-        <h1 class="bg-red-100">Start of Movie Rater Home</h1>
+        
+        <div class="flex flex-col bg-black">
+          <Navbar></Navbar>
+          <div class="flex-grow container mx-auto py-4">
+            
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+              
+            <img class="w-full" src={img} alt="Mulan"/>
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Mulan</div>
+                
+              </div>
+              
+
+            </div>
+
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+              
+            <img class="w-full" src={img2} alt="Soul"/>
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Soul</div>
+                
+              </div>
+              
+
+            </div>
+          </div>
+          <Footer/>
+        </div>
       </div>
     );
   }
