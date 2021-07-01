4aa433f60b595659c3ae1ce89003869d74a8322e
rich5k <richardanatsui@gmail.com>
2021-07-01 01:19:33 +0100

    added hover effect and grid to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,30 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
+
 import './tailwindcss/styles.css';
 import img from './img/mulan.jpg';
 import img2 from './img/soul.jpg';
 function Home() {
+  
+    function changeOpacity(e){
+      e.target.style.opacity = 0.5;
+    }
+    function revertOpacity(e){
+      e.target.style.opacity = 1.0;
+    }
+
     return (
       <div className="Home">
         
         <div class="flex flex-col bg-black">
           <Navbar></Navbar>
-          <div class="flex-grow container mx-auto py-4">
+          <div class="flex-grow container mx-auto py-4 grid lg:grid-cols-3 sm:grid-cols-2">
             
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white m-4 ">
               
-            <img class="w-full" src={img} alt="Mulan"/>
+            <img class="w-full" src={img} alt="Mulan" 
+            onMouseEnter={changeOpacity}
+            onMouseLeave={revertOpacity}/>
               <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Mulan</div>
                 
@@ -22,9 +33,37 @@ function Home() {
 
             </div>
 
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white">
               
-            <img class="w-full" src={img2} alt="Soul"/>
+            <img class="w-full" src={img2} alt="Soul"
+            onMouseEnter={changeOpacity}
+            onMouseLeave={revertOpacity}/>
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Soul</div>
+                
+              </div>
+              
+
+            </div>
+
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white m-4 ">
+              
+            <img class="w-full" src={img} alt="Mulan"
+            onMouseEnter={changeOpacity}
+            onMouseLeave={revertOpacity}/>
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Mulan</div>
+                
+              </div>
+              
+
+            </div>
+
+            <div class="max-w-sm rounded overflow-hidden shadow-lg m-4 bg-white">
+              
+            <img class="w-full" src={img2} alt="Soul"
+            onMouseEnter={changeOpacity}
+            onMouseLeave={revertOpacity}/>
               <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Soul</div>
                 
