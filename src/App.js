fb3ac227175df8ee96cfd15e9971c00e92b90b01
rich5k <richardanatsui@gmail.com>
2021-07-09 00:27:47 +0100

    added add and sign in pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Home from './Home';
-
+import Add from './Add';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import SignIn from './SignIn';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <SignIn/> */}
+      <Router>
+          <Switch>
+          <Route path="/signin">
+                <SignIn />
+            </Route>
+            <Route path="/add">
+                <Add />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+          </Switch>
+      </Router>
     </div>
+    
   );
 }
 
