eb889f0d38067b767e84588e71fbcc435d94a7b4
rich5k <richardanatsui@gmail.com>
2021-07-09 10:06:22 +0100

    fixed overlay problem

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,10 @@
 import Footer from './Footer';
 import Navbar from './Navbar';
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import './tailwindcss/styles.css';
 import img from './img/mulan.jpg';
 import img2 from './img/soul.jpg';
+import './Home.css';
 function Home() {
   
     function changeOpacity(e){
@@ -11,6 +12,13 @@ function Home() {
     }
     function revertOpacity(e){
       e.target.style.opacity = 1.0;
+    }
+
+    const history = useHistory();
+
+    const routeChange = ()=>{
+      let path = '/viewmore';
+      history.push(path);
     }
 
     return (
@@ -28,11 +36,25 @@ function Home() {
           </div>
           <div class="flex-grow container mx-auto py-4 grid lg:grid-cols-3 sm:grid-cols-2">
             
-            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white m-4 ">
+            <div class="content max-w-sm rounded overflow-hidden shadow-lg bg-white m-4 ">
+            <div class="content-overlay"></div>
+            <img class="w-full content-image" src={img} alt="Mulan" />
+            <div class="content-details fadeIn-bottom">
+              <h3 class="content-title">
+                <svg xmlns="http://www.w3.org/2000/svg" className="text-yellow-300 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg> 
+                  4.0/5.0</h3>
+              <p class="content-text">This is a short description</p>
+              <button class="my-2 bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full">
+                    Rate
+                  </button>
               
-            <img class="w-full" src={img} alt="Mulan" 
-            onMouseEnter={changeOpacity}
-            onMouseLeave={revertOpacity}/>
+              <br></br>
+                  <button onClick={routeChange} class=" my-4 bg-transparent hover:bg-green-400 text-green-600 font-semibold hover:text-white py-2 px-4 border border-green-400 hover:border-transparent rounded">
+                    View More
+                  </button>
+            </div>
               <div class="px-6 py-4">
                 <div class="font-bold text-xl mb-2">Mulan</div>
                 
@@ -61,7 +83,7 @@ function Home() {
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                   </svg> 4.0/5.0
                   </h1>
-                  <button class="bg-green-300 hover:bg-green-500 text-white font-bold py-2 px-4 rounded-full">
+                  <button class="bg-green-300 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full">
                     Rate
                   </button>
                   <br></br>
