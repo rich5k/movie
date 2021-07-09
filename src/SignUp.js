549cf459bb2550d0b3f1b911568709cf19fc67be
rich5k <richardanatsui@gmail.com>
2021-07-10 00:24:12 +0100

    added signup page

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,3 +1,4 @@
+import './tailwindcss/styles.css';
 const SignUp = () => {
     return ( 
         <div className="SignUp">
@@ -6,55 +7,57 @@ const SignUp = () => {
                     <div>
                     <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow"/>
                     <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Sign in to your account
+                        Sign Up your account
                     </h2>
-                    <p class="mt-2 text-center text-sm text-gray-600">
-                        Or
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                        start your 14-day free trial
-                        </a>
-                    </p>
+                    
                     </div>
                     <form class="mt-8 space-y-6" action="#" method="POST">
                     {/* <input type="hidden" name="remember" value="true"/> */}
-                    <div class="rounded-md shadow-sm -space-y-px">
-                        <div>
-                        <label for="email-address" class="">Email address</label>
-                        <input id="email-address" name="email" type="email"  required class="appearance-none rounded-none relative  w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
-                        </div>
-                        <div>
-                        <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password"  required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
-                        </div>
-                    </div>
+                    <input 
+                        type="text"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="fullname"
+                        placeholder="Full Name" />
 
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"/>
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-                            Remember me
-                        </label>
-                        </div>
+                    <input 
+                        type="email"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
 
-                        <div class="text-sm">
-                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                            Forgot your password?
+                    <input 
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <input 
+                        type="password"
+                        class="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="confirm_password"
+                        placeholder="Confirm Password" />
+
+                    <button
+                        type="submit"
+                        class="w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none my-1"
+                    >Create Account</button>
+
+                    <div class="text-center text-sm text-grey-700 mt-4">
+                        By signing up, you agree to the  
+                        <a class="no-underline border-b border-grey-700 text-grey-700" href="#">
+                             Terms of Service
+                        </a> and 
+                        <a class="no-underline border-b border-grey-700 text-grey-700" href="#">
+                            Privacy Policy
                         </a>
-                        </div>
                     </div>
 
-                    <div>
-                        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                            {/* <!-- Heroicon name: solid/lock-closed --> */}
-                            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
-                        Sign in
-                        </button>
-                    </div>
                     </form>
+                    <div class="text-grey-700 mt-6">
+                        Already have an account? 
+                        <a class="no-underline border-b border-blue-500 text-blue-500" href="../login/">
+                            Log in
+                        </a>.
+                    </div>
                 </div>
                 </div>
         </div>
