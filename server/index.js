137fe1de22ec646287ce1a200cdbc95490da519a
rich5k <richardanatsui@gmail.com>
2021-07-29 18:45:57 +0100

    added the router to server/index file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const db = require('./db');
+const movieRouter = require('./routes/movie-router');
+const movieRatingRouter = require('./routes/movieRating-router');
+const userRouter = require('./routes/user-router');
 
 const app= express();
 const apiPort = 3000;
@@ -16,5 +19,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
+
+app.use('/api',movieRouter);
+app.use('/api',movieRatingRouter);
+app.use('/api',userRouter);
 
 app.listen(apiPort, ()=>console.log(`Server running on port ${apiPort}`));
