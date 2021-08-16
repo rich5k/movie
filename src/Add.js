9ad924e59146e7c099c8195ed16b7d8e92661ab6
rich5k <richardanatsui@gmail.com>
2021-08-16 18:11:48 +0100

    fixed value file problem in add form

--- src/Add.js
+++ src/Add.js
@@ -85,7 +85,6 @@ const Add = () => {
                     <label className="block text-sm font-medium text-gray-700">
                         Cover photo
                     </label>
-                    <input id="image" name="image" required  onChange={(e)=> setImage(e.target.files[0])} type="file" className="sr-only" />
                     <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                         <div className="space-y-1 text-center">
                         <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -94,7 +93,7 @@ const Add = () => {
                         <div className="flex text-sm text-gray-600">
                             <label for="image" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                             <span>Upload a file</span>
-                            <input id="image" name="image" required value={image} onChange={(e)=> setImage(e.target.files[0])} type="file" className="sr-only" />
+                            <input id="image" name="image" required onChange={(e)=> setImage(e.target.files[0])} type="file" className="sr-only" />
                             </label>
                             <p className="pl-1">or drag and drop</p>
                         </div>
