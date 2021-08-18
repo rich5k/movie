bc81174a284e6488f71ec12071c0737e5e946270
rich5k <richardanatsui@gmail.com>
2021-08-18 10:02:54 +0100

    changed the directory of the upload folder

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const multer= require('multer');
 const storage = multer.diskStorage({
     destination : (req, file, cb)=> {
-        cb(null, 'uploads')
+        cb(null, '../public/uploads')
     },
     filename: (req,file,cb)=>{
         // cb(null, Date.now() + '-'+ file.originalname)
