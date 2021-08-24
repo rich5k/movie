c552bea7391f3d25942d74f7b5d080f1ae243a57
rich5k <richardanatsui@gmail.com>
2021-08-24 14:18:14 +0100

    added getUserByEmail to user controller

--- server/controllers/user-ctrl.js
+++ server/controllers/user-ctrl.js
@@ -164,6 +164,21 @@ const getUserById = async (req, res)=>{
     }).catch(err=> console.log(err))
 }
 
+const getUserByEmail = async (req, res)=>{
+    await User.findOne({email: req.body.email}, (err,user)=>{
+        if(err){
+            return res.status(400).json({success: false, error: err})
+        }
+
+        if (!user){
+            return res
+                .status(404)
+                .json({success: false, error: 'User not found'})
+        }
+        return res.status(200).json({success: true, data: user})
+    }).catch(err=> console.log(err))
+}
+
 const getUsers = async (req, res)=>{
     await User.find({}, (err, users)=>{
         if(err){
@@ -184,4 +199,5 @@ module.exports ={
     deleteUser,
     getUsers,
     getUserById,
+    getUserByEmail
 }
