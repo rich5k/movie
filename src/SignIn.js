5d6fa7448b56b17bdcd8ea9793eea3dc85140ddd
rich5k <richardanatsui@gmail.com>
2021-08-26 12:02:19 +0100

    hooked up the sign in form to the backend

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React,{ useState } from "react";
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+import {API_URL} from './config';
 const SignIn = () => {
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState(undefined);
+    const submitForm=(e)=>{
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('email', email);
+        formData.append('password', password);
+        try{
+            const response =axios.post(`${API_URL}/api/user/email`, formData);
+            console.log('Returned data:',response);
+            // history.push('/login');
+        }
+        catch(e){
+            console.log(`Axios request failed: ${e}`)
+        }
+    }
     return ( 
         <div className="SignIn">
             <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
@@ -21,11 +40,11 @@ const SignIn = () => {
                     <div class="rounded-md shadow-sm -space-y-px">
                         <div>
                         <label for="email-address" class="sr-only">Email address</label>
-                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required value={email} onChange={(e)=> setEmail(e.target.value)} class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address"/>
                         </div>
                         <div>
                         <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required value={password} onChange={(e)=> setPassword(e.target.value)} class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password"/>
                         </div>
                     </div>
 
@@ -45,7 +64,7 @@ const SignIn = () => {
                     </div>
 
                     <div>
-                        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <button type="submit"  onClick={submitForm} class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         <span class="absolute left-0 inset-y-0 flex items-center pl-3">
                             {/* <!-- Heroicon name: solid/lock-closed --> */}
                             <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
