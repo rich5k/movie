99417ac231a4ad9afe528ad8e844d3359124d4b1
rich5k <richardanatsui@gmail.com>
2021-08-28 10:39:02 +0100

    added avgRating function to Movielist

--- src/MovieList.js
+++ src/MovieList.js
@@ -21,6 +21,9 @@ const MovieList = ({movies}) => {
       console.log('ID: '+movie);
     }
 
+    const avgRatings=(id)=>{
+
+    }
     
     console.log(movies.data);
     return ( 
@@ -36,7 +39,7 @@ const MovieList = ({movies}) => {
                             <div class="flex items-center justify-center">
                             <svg class="mx-4 w-4 h-4 fill-current text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
                             <h3 class="content-title   ">
-                                4.0/5.0</h3>
+                                {avgRatings(movie._id)}/5.0</h3>
                             </div>
                             <p class="content-text">{movie.desc}</p>
                             <button onClick={routeChange2.bind(null,movie._id)} class="my-2 bg-green-400 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full">
